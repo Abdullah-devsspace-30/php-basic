0d5602069a9143ca451123b8a3e31a14c08c1daa
Abdullah-devsspace-30 <muhammad.abdullah@devsspace.com>
2024-12-02 18:26:30 +0500

    ADD : class and object

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
 
 
-class Person{
+class Person
+{
     public $id;
     public $name;
     public $email;
-    public function __construct($id,$name,$email) {
+    public function __construct($id, $name, $email)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
     }
 
-    public function display()   
+    public function display()
     {
-        echo "EMPLOYEE ID : " . $this->id ."<br/>";
-        echo "NAME : " . $this->name ."<br/>";
-        echo "EMAIL : " . $this->email ."<br/>";
+        echo "EMPLOYEE ID : " . $this->id . "<br/>";
+        echo "NAME : " . $this->name . "<br/>";
+        echo "EMAIL : " . $this->email . "<br/>";
     }
 
     // class object
 
 }
-$person = new Person(20025,"Muhammad Abdullah Baig","bsce20025@itu.edu.pk");
+$person = new Person(20025, "Muhammad Abdullah Baig", "bsce20025@itu.edu.pk");
 $person->display();
 
 
@@ -70,24 +72,24 @@ echo $bool;
 echo "<br/>";
 
 
-echo "**** understanding OOP **** <br/>"; 
+echo "**** understanding OOP **** <br/>";
 echo "<br/>";
 
 echo "For Loop <br/>";
 echo "<br/>";
 
 
-for($i =1 ;$i<=10;$i++){
-    echo "index : ". $i;
+for ($i = 1; $i <= 10; $i++) {
+    echo "index : " . $i;
     echo "<br/>";
 }
 
 echo "While Loop <br/>";
 echo "<br/>";
 
-$w =1;
-while($w <= 10){
-    echo "while loop value of index : ". $w;
+$w = 1;
+while ($w <= 10) {
+    echo "while loop value of index : " . $w;
     $w++;
     echo "<br/>";
 }
@@ -95,7 +97,7 @@ echo "<br/>";
 echo "For Each Loop <br/>";
 echo "<br/>";
 
-$av = ["new","old","between"];
+$av = ["new", "old", "between"];
 foreach ($av as $key) {
     echo " --> " . $key;
     echo "<br/>";
@@ -104,11 +106,11 @@ foreach ($av as $key) {
 echo "<br/>";
 echo "do while Loop <br/>";
 echo "<br/>";
-$a =1;
+$a = 1;
 do {
-   echo "do while loop value of index : ". $a;
-   $a++;
-   echo "<br/>";
+    echo "do while loop value of index : " . $a;
+    $a++;
+    echo "<br/>";
 } while ($a <= 10);
 
 echo "<br/>";
@@ -117,4 +119,3 @@ echo "**** end ***** <br/>";
 
 
 echo "***** OOP IN PHP ***** <br/>";
-
