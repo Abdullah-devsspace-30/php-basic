13db0129eddf8a7dff22d0523db86678f2d1de7d
Abdullah-devsspace-30 <muhammad.abdullah@devsspace.com>
2024-12-02 18:13:51 +0500

    all loops practice

--- index.php
+++ index.php
@@ -19,3 +19,47 @@ echo "<br/>";
 $bool = false;
 echo $bool;
 echo "<br/>";
+
+
+echo "**** understanding OOP **** <br/>"; 
+echo "<br/>";
+
+echo "For Loop <br/>";
+echo "<br/>";
+
+
+for($i =1 ;$i<=10;$i++){
+    echo "index : ". $i;
+    echo "<br/>";
+}
+
+echo "While Loop <br/>";
+echo "<br/>";
+
+$w =1;
+while($w <= 10){
+    echo "while loop value of index : ". $w;
+    $w++;
+    echo "<br/>";
+}
+echo "<br/>";
+echo "For Each Loop <br/>";
+echo "<br/>";
+
+$av = ["new","old","between"];
+foreach ($av as $key) {
+    echo " --> " . $key;
+    echo "<br/>";
+}
+
+echo "<br/>";
+echo "do while Loop <br/>";
+echo "<br/>";
+$a =1;
+do {
+   echo "do while loop value of index : ". $a;
+   $a++;
+   echo "<br/>";
+} while ($a <= 10);
+
+echo "**** end ***** <br/>";
